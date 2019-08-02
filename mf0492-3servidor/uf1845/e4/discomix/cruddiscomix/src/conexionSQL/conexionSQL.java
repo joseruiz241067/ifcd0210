14edcf64367252cc393ffb73d6c877a26544dea0
jose241067 <joserg77@hotmail.com>
2019-08-02 08:59:31 +0100

    02/08/2019

---
+++ mf0492-3servidor/uf1845/e4/discomix/cruddiscomix/src/conexionSQL/conexionSQL.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionSQL;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author USUARIO PRINCIPAL
+ */
+public class conexionSQL {
+    
+    
+    Connection conectar=null;
+    public Connection conexion () {
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/discomix", "root", "");
+            
+            JOptionPane.showMessageDialog(null,"Conexion Exitosa");
+            
+                     
+            
+        } catch (Exception e) {
+           JOptionPane.showMessageDialog(null,"Error Conexion" + e.getMessage()); 
+        }
+        return  conectar;
+        
+    }
+    
+      
+    
+}
